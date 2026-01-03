56838d627d1b77d6f24504b9d83f14b830d2b2a7
Muhammad Saqlain <msaqlain.official05@gmail.com>
2026-01-03 21:38:15 +0400

    fix playwright issue

---
+++ tests/dropdown.spec.js
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test'
+
+test('Verify dropdown functionality', async ({ page }) => {
+  await page.goto('https://freelance-learn-automation.vercel.app/signup');
+  
+  // Add your dropdown test cases here
+  // For example, selecting options from dropdowns and verifying selections
+  
+ 
+});
